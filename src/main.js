8af04f6d951511f8ce3bb7469a81ff7d027d8fb4
ioan-dev <82202528+tumanoviv@users.noreply.github.com>
2025-06-13 20:40:41 +0300

    git init

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// src/main.js
+import Header from "./modules/Header.js";
+import Accordion from "./modules/Accordion.js";
+import './styles/main.scss';
+
+new Header();
+new Accordion({singleActive: false});
+
+
+
+
+
