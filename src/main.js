8491983377b7e0db52952277c1703f64b5673870
ioan-dev <82202528+tumanoviv@users.noreply.github.com>
2025-07-12 15:33:29 +0300

    Update system

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-// src/main.js
+import '@/styles/main.scss';
 import Header from "./modules/Header.js";
-import Accordion from "./modules/Accordion.js";
-import './styles/main.scss';
 
 new Header();
-new Accordion({singleActive: false});
+
 
 
 
